4e55aec28f7522ee2614655cae303877ec5b7d37
Kartik Subramanian <karu@Kartiks-MacBook-Pro-3.local>
2023-07-30 17:46:21 -0500

    update latency

--- src/latency.cpp
+++ src/latency.cpp
@@ -1,4 +1,3 @@
-#include <chrono>
 #include <cstring>
 #include <unistd.h>
 #include <iostream>
@@ -7,7 +6,17 @@
 #include "RApiPlus.h"
 
 int MdLoggedIn = 0;
+int TsLoggedIn = 0;
+
 RApi::REngine *pEngine;
+RApi::LimitOrderParams order;
+
+long long tickTs = 0;
+
+bool cmp(tsNCharcb a, tsNCharcb b)
+{
+    return (a.iDataLen == b.iDataLen) && (strncmp(a.pData, b.pData, a.iDataLen) == 0);
+}
 
 class AdmCallbacks : public RApi::AdmCallbacks
 {
@@ -24,6 +33,7 @@ public:
     ~Callbacks() {}
     virtual int Alert(RApi::AlertInfo *pInfo, void *pContext, int *aiCode);
     virtual int TradePrint(RApi::TradeInfo *pInfo, void *pContext, int *aiCode);
+    virtual int LineUpdate(RApi::LineInfo *pInfo, void *pContext, int *aiCode);
 };
 
 int AdmCallbacks::Alert(RApi::AlertInfo *pInfo, void *pContext, int *aiCode)
@@ -47,23 +57,67 @@ int Callbacks::Alert(RApi::AlertInfo *pInfo, void *pContext, int *aiCode)
             MdLoggedIn = 1;
         }
     }
+    else if (pInfo->iConnectionId == RApi::TRADING_SYSTEM_CONNECTION_ID)
+    {
+        if (pInfo->iAlertType == RApi::ALERT_LOGIN_FAILED)
+        {
+            TsLoggedIn = -1;
+        }
+        else if (pInfo->iAlertType == RApi::ALERT_LOGIN_COMPLETE)
+        {
+            TsLoggedIn = 1;
+        }
+    }
 
     *aiCode = API_OK;
     return OK;
 }
 
+// int Callbacks::AccountList(RApi::AccountListInfo *pInfo, void *pContext, int *aiCode)
+// {
+//     int iCode;
+//     pInfo->dump(&iCode);
+//     if (pInfo->iArrayLen > 0)
+//     {
+//         RApi::AccountInfo *pAccount = &pInfo->asAccountInfoArray[0];
+//         memccpy(g_cAccountId, pAccount->sAccountId.pData, 0, g_iMAX_LEN);
+//         memccpy(g_cIbId, pAccount->sIbId.pData, 0, g_iMAX_LEN);
+//         memccpy(g_cFcmId, pAccount->sFcmId.pData, 0, g_iMAX_LEN);
+//         ReceivedAccount = 1;
+//     }
+//     else
+//     {
+//         ReceivedAccount = -1;
+//     }
+//     *aiCode = API_OK;
+//     return OK;
+// }
+
 int Callbacks::TradePrint(RApi::TradeInfo *pInfo, void *pContext, int *aiCode)
 {
-    long long recv = std::chrono::high_resolution_clock::now().time_since_epoch().count();
-    long long source = pInfo->iSourceSsboe;
-    source = (source * 1000 * 1000) + pInfo->iSourceUsecs;
-    source = (source * 1000) + pInfo->iSourceNsecs;
-    
-    std::cout << source << std::endl;
-    std::cout << recv << std::endl;
-    std::cout << recv - source << " nanos" << std::endl;
-    std::cout << std::endl; // << recv - source << " micros"
+    tickTs = pInfo->iSsboe;
+    tickTs = (tickTs * 1000 * 1000) + pInfo->iUsecs;
 
+    order.dPrice = pInfo->dPrice - 10;
+
+    int iCode;
+    if (!pEngine->sendOrder(&order, &iCode))
+    {
+        std::cerr << "error placing order: " << iCode << std::endl;
+    }
+
+    *aiCode = API_OK;
+    return OK;
+}
+
+int Callbacks::LineUpdate(RApi::LineInfo *pInfo, void *pContext, int *aiCode)
+{
+    if (cmp(pInfo->sStatus, RApi::sLINE_STATUS_ORDER_SENT_TO_EXCH))
+    {
+        long long tradeTs = pInfo->iSsboe;
+        tradeTs = (tradeTs * 1000 * 1000) + pInfo->iUsecs;
+        std::cout << tradeTs - tickTs << " micros" << std::endl;
+    }
     *aiCode = API_OK;
     return OK;
 }
@@ -96,7 +150,7 @@ int main(int argc, char **argv)
     }
 
     RApi::REngineParams oParams;
-    oParams.sAppName = {"latency", (int)strlen("latency")};
+    oParams.sAppName = {"luper", (int)strlen("luper")};
     oParams.sAppVersion = {version, (int)strlen(version)};
     oParams.envp = Envp;
     oParams.pAdmCallbacks = pAdmCallbacks;
@@ -132,6 +186,9 @@ int main(int argc, char **argv)
     oLoginParams.sMdUser = {username, (int)strlen(username)};
     oLoginParams.sMdPassword = {password, (int)strlen(password)};
     oLoginParams.sMdCnnctPt = {MdConnectPt, (int)strlen(MdConnectPt)};
+    oLoginParams.sTsUser = {username, (int)strlen(username)};
+    oLoginParams.sTsPassword = {password, (int)strlen(password)};
+    oLoginParams.sTsCnnctPt = {TsConnectPt, (int)strlen(TsConnectPt)};
 
     if (!pEngine->login(&oLoginParams, &iCode))
     {
@@ -156,8 +213,46 @@ int main(int argc, char **argv)
         return BAD;
     }
 
-    tsNCharcb sExchange = {"CME", 3};
+    while (TsLoggedIn == 0)
+    {
+        sleep(1);
+
+    }
+
+    if (TsLoggedIn == -1)
+    {
+        std::cerr << "login to ts failed, check user/pass or permissions" << std::endl;
+        delete pEngine;
+        delete pCallbacks;
+        delete pAdmCallbacks;
+        return BAD;
+    }
+
+    RApi::AccountInfo pAccount;
+    pAccount.sIbId = {"Ironbeam", (int)strlen("Ironbeam")};
+    pAccount.sFcmId = {"Ironbeam", (int)strlen("Ironbeam")};
+    pAccount.sAccountId = {username, (int)strlen(username)};
+
+    if (!pEngine->subscribeOrder(&pAccount, &iCode))
+    {
+        std::cerr << "error subscribing to orders: " << iCode << std::endl;
+        delete pEngine;
+        delete pCallbacks;
+        delete pAdmCallbacks;
+    }
+
     tsNCharcb sTicker = {ticker, (int)strlen(ticker)};
+    tsNCharcb sExchange = {"CME", (int)strlen("CME")};
+    tsNCharcb sTradeRoute = {"globex", (int)strlen("globex")};
+    
+    order.iQty = 1;
+    order.sTicker = sTicker;
+    order.sExchange = sExchange;
+    order.pAccount = &pAccount;
+    order.sTradeRoute = sTradeRoute;
+    order.sDuration = RApi::sORDER_DURATION_FOK;
+    order.sBuySellType = RApi::sBUY_SELL_TYPE_BUY;
+    order.sEntryType = RApi::sORDER_ENTRY_TYPE_AUTO;
 
     int iFlags = (RApi::MD_PRINTS);
 
